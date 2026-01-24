2571f5a7da23a7a08b05e0af7ed33fb06db2e664
Роман Нагаєвський <nagaewsky.roma.s@gmail.com>
2026-01-24 11:57:16 +0200

    ++

--- src/bot/handlers/startOver.handler.js
+++ src/bot/handlers/startOver.handler.js
@@ -1,35 +1,3 @@
-// import { STEPS } from "../../core/fsm/steps.js";
-// // import { goToStep } from "../../core/fsm/transition.js";
-// import { getSession, resetSession } from "../../utils/helpers.js";
-// import { setStep } from "../../core/fsm/transition.js";
-// import { renderStep } from "../render/renderStep.js";
-
-// export async function startOverHandler(ctx) {
-//   console.log("🔄 startOverHandler"); //test
-
-//   resetSession(ctx.chat.id); // Скидаємо сесію користувача
-
-//   const chatId =
-//     ctx.chat?.id ??
-//     ctx.callbackQuery?.message?.chat?.id ??
-//     ctx.update?.callback_query?.message?.chat?.id;
-
-//   const session = getSession(chatId);
-
-//   // Встановлюємо крок на SERVICE
-//   setStep(session, STEPS.SERVICE);
-
-//   session.data.fullName ??=
-//     ctx.from.first_name + (ctx.from.last_name ? " " + ctx.from.last_name : "");
-
-//   // безпечно відповісти на callback, якщо він є
-//   if (ctx.callbackQuery) await ctx.answerCbQuery(); // це і перезаписує ?
-
-//   return renderStep(ctx, session);
-// }
-
-//========================================================================================================================================================
-
 import { Markup } from "telegraf";
 import { STEPS } from "../../core/fsm/steps.js";
 import { getSession, resetSession } from "../../utils/helpers.js";
@@ -63,8 +31,10 @@ export async function startOverHandler(ctx) {
   // якщо хочеш кнопку назад/на початок — додай
   // buttons.push([Markup.button.callback("↩️ На початок", "START_OVER")]);
 
+  await ctx.reply("Ок! Починаємо 🆕", Markup.removeKeyboard()); // пробую видалити кнопку
+
   return ctx.reply(
     "🆕 Новий запис. Оберіть послугу:",
-    Markup.inlineKeyboard(buttons)
+    Markup.inlineKeyboard(buttons),
   );
 }
