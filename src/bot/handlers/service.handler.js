53237815d94fc10136f74ca665c1d27ecf2914af
Роман Нагаєвський <nagaewsky.roma.s@gmail.com>
2026-02-16 17:05:16 +0200

    ++

--- src/bot/handlers/service.handler.js
+++ src/bot/handlers/service.handler.js
@@ -52,7 +52,7 @@ export async function serviceHandler(ctx) {
   // 2️⃣ змінюємо стан з допомогою transition
   // спеціальна логіка для типу клієнта
   if (service.id === "wash_contract") {
-    session.data.prices = []; // очищаємо прайс чи краще оновити? , бо для контракту будуть другі ціни
+    //session.data.prices = []; // очищаємо прайс чи краще оновити? , бо для контракту будуть другі ціни
     // session.data.prices = await sheetsApi.contractPricingGet(); // оновлюємо прайс для контракту (якщо він відрізняється від рітейлу)
     session.data.clientType = "contract";
     goToStep(session, STEPS.CONTRACT_NO);
